39af8aa1c9b6e91ad2e50e12c2abe5500a1db26f
rebeccaSimmonds19 <rmsimmonds1919@gmail.com>
2019-05-23 16:43:00 +0100

    changed ef error

--- wsgi.py
+++ wsgi.py
@@ -15,23 +15,24 @@ import json
 app = Flask(__name__)
 @app.route('/')
 def index():
-    def plot():
-        data = pd.read_csv("wine-reviews/winemag-data_first150k.csv")
-        temp = data["points"].value_counts()
-        data = [go.Bar(
-                x=temp.index,
-                y=temp.values,
-                marker=dict(
-                color='purple'
-                )
-        )]
-        layout = go.Layout(
-            autosize=True,
-            title="feature selection"
-    )
 
-        figure = go.Figure(data=data, layout=layout)
-        return offline.plot(figure)
+    data = pd.read_csv("wine-reviews/winemag-data_first150k.csv")
+    temp = data["points"].value_counts()
+    data = [go.Bar(
+            x=temp.index,
+            y=temp.values,
+            marker=dict(
+            color='purple'
+            )
+    )]
+    layout = go.Layout(
+        autosize=True,
+        title="feature selection"
+        )
+
+    figure = go.Figure(data=data, layout=layout)
+    print('offline:  '+offline.plot(figure))
+    return 'hello'
 
 if __name__ == '__main__':
     app.run()
