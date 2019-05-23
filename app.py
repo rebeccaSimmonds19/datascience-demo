c608e75d70a831108687be549a22b02250db8419
rebeccaSimmonds19 <rmsimmonds1919@gmail.com>
2019-05-23 08:58:19 +0100

    trying the plotly lib on its own to display the graph in the browser on os it works locally

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from sklearn.model_selection import train_test_split
 from catboost import Pool, CatBoostRegressor, cv
 import pandas as pd
-import matplotlib.pyplot as plt
-from plotly.offline import plot
+import plotly.plotly
+import plotly.offline as offline
 import plotly.graph_objs as go
 import seaborn as sns
 import os
@@ -10,12 +10,12 @@ from os import environ
 import shutil
 import argparse
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
-data = pd.read_csv("wine-reviews/winemag-data_first150k.csv")
-
 def plot():
+    data = pd.read_csv("wine-reviews/winemag-data_first150k.csv")
     temp = data["points"].value_counts()
     data = [go.Bar(
             x=temp.index,
@@ -27,28 +27,17 @@ def plot():
     layout = go.Layout(
         autosize=True,
         title="feature selection"
+)
 
-    )
-    #sns.barplot(x = x, y=y, data=data)
-    #plt.show(filename="feature.html")
-    fig = go.Figure(data=data, layout=layout)
-    print(plot(fig, filename='features.html'))
+    figure = go.Figure(data=data, layout=layout)
+    return offline.plot(figure)
 
-def make_template():
-    # make the templates dir
-    new_path = '/opt/app-root/src/templates'
-    if not os.path.exists(new_path):
-        os.makedirs(new_path)
-        # move the file to the templates dir
-        shutil.move('/opt/app-root/src/feature.html', new_path)
-    return render_template("features.html", title='Feature selection')
 
 @app.route('/')
 def index():
-    plot()
-    return make_template()
+    return plot()
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 9999))
     app.run(host='0.0.0.0', port=port)
